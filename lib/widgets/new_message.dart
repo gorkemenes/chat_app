a20698f932c1dbbfcd8b52dbf29c68e541e2c368
Görkem Enes İNCİ <gorkemenesinci@gmail.com>
2024-05-15 13:56:39 +0300

    Deneme

---
+++ lib/widgets/new_message.dart
@@ -0,0 +1,65 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class NewMessage extends StatefulWidget {
+  const NewMessage({super.key});
+
+  @override
+  State<NewMessage> createState() => _NewMessageState();
+}
+
+class _NewMessageState extends State<NewMessage> {
+  final messageController = TextEditingController();
+
+  @override
+  void dispose() {
+    messageController.dispose();
+    super.dispose();
+  }
+
+  void _submitMessage() async {
+    final enteredMessage = messageController.text;
+
+    if (enteredMessage.trim().isEmpty) {
+      return;
+    }
+    FocusScope.of(context).unfocus();
+    messageController.clear();
+
+    final user = FirebaseAuth.instance.currentUser!;
+    final userData = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(user.uid)
+        .get();
+    await FirebaseFirestore.instance.collection('chat').add({
+      'text': enteredMessage,
+      'createdAt': Timestamp.now(),
+      'userId': user.uid,
+      'username': userData.data()!['username'],
+      'userImage': userData.data()!['image_url']
+    });
+    // send firebase
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 15, right: 1, bottom: 14),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: messageController,
+            ),
+          ),
+          IconButton(
+              color: Theme.of(context).colorScheme.primary,
+              onPressed: _submitMessage,
+              icon: const Icon(Icons.send)),
+        ],
+      ),
+    );
+  }
+}
